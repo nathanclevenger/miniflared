7b7ed81732b31b9234595bcf4c8891e07f466c1a
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-01-15 13:15:53 -0600

    Got it running!

--- worker.js
+++ worker.js
@@ -1,15 +1,18 @@
-// export default {
-//   fetch: req => fetch('https://ctx.do/api')
-// }
+export default {
+  fetch: req => {
+    const { pathname, search } = new URL(req.url)
+    if (pathname == '/api') {
+      return new Response(JSON.stringify({ message: 'Hello World!' }), {'content-type': 'application/json' })
+    } else {
+      const url = pathname == '/' ? 'https://ctx.do/api' : 'https:/' + pathname + search
+      return fetch(url, { headers: {
+        'referer': new URL(url).origin,
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+      } })
+    }
+  }
+}
 
 const headers = {
   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
 }
-
-addEventListener("fetch", (event) => {
-  const req = new Request(event.request)
-  // req.headers.delete('mf-loop')
-  // event.respondWith(fetch('https://postman-echo.com/get?foo1=bar1&foo2=bar2', { headers })) //, event.request))
-  // event.respondWith(fetch('https://ctx.do/api', { headers })) //, event.request))
-  event.respondWith(new Response(JSON.stringify({ hello: 123}), { headers: { 'content-type': 'application/json' } }))
-})
